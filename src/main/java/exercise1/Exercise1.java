a4220147c951ef0b1b413039e43e8ca661b1176e
OlivierDM1999 <181964@umons.ac.be>
2019-10-23 15:02:49 +0200

    Premier exercice IZI

--- src/main/java/exercise1/Exercise1.java
+++ src/main/java/exercise1/Exercise1.java
@@ -1,14 +1,20 @@
 package exercise1;
 
 public class Exercise1 {
-	static Person createPerson(String name, int age) {
+	static Person createPerson(String name, int age) { //utilisée par les tests unitaires
+		Person P=new Person(name,age);
 		// Ajoutez les champs name et age à la classe Person.
 		// Créez un constructeur public permettant d'initialiser ces valeurs au moment de la construction.
 		// Créez des getters publics pour lire ces valeurs une fois la classe construite.
-		return null;
+		return P;
 	}
 
 	public static void main(String[] args) {
+		String nom=args[0];
+		String ag=args[1];
+		int age=Integer.parseInt(ag);
+		Person P=createPerson(nom,age);
+		P.afficher();
 		// Considérant que cette classe est démarrée en ligne de commande avec un premier paramètre donnant le nom
 		// et un second donnant l'âge (nombre entier), créez un objet Person sur base de ceux-ci.
 		// Ecrivez ensuite le nom et l'âge de cette personne sur la sortie standard.
--- src/main/java/exercise1/Person.java
+++ src/main/java/exercise1/Person.java
@@ -1,4 +1,37 @@
 package exercise1;
 
 class Person {
+	private String name;
+	private int age;
+
+	public Person (String nom,int age)
+	{
+		this.name=nom;
+		this.age=age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public void setName(String nom) {
+		this.name = nom;
+	}
+
+	public int getAge()
+	{
+		return this.age;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void afficher()
+	{
+		System.out.println("Nom : "+this.name+", Age : "+this.age);
+	}
+
+
+
 }
