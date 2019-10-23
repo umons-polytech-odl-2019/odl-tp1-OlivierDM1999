b0869758181e00f16a694aa59721e412af0c3b24
OlivierDM1999 <181964@umons.ac.be>
2019-10-23 15:29:44 +0200

    Deuxième exercice SUCESS

--- src/main/java/exercise2/Person.java
+++ src/main/java/exercise2/Person.java
@@ -1,4 +1,37 @@
 package exercise2;
 
 class Person {
+	private int age;
+	private static int totalAge=0;
+	private static int populationSize=0;
+
+	public Person(int age)
+	{
+		this.age=age;
+		totalAge=totalAge+this.age;
+		populationSize++;
+	}
+
+	public static int computePopulationSize(){
+
+		return populationSize;
+	}
+
+	public static float computeAveragePopulationAge()
+	{
+		return totalAge/populationSize;
+	}
+
+	public static void resetPopulation ()
+	{
+		totalAge=0;
+		populationSize=0;
+	}
+
+
+
+
+
+
 }
+
